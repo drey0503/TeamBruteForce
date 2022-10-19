7cfa9b594f462ee422448a22f1c82ee4bbb4c06b
Dreyson <dreyson@email.sc.edu>
2022-10-19 14:03:43 -0400

    working on transfer between screens in the menu

--- app/src/main/java/com/example/tracknba/Chat.java
+++ app/src/main/java/com/example/tracknba/Chat.java
@@ -1,4 +1,18 @@
 package com.example.tracknba;
 
-public class Chat {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Chat extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat);
+    }
+
 }
