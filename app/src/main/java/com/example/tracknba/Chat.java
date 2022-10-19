8c93be74aa0968104b67e77d7e561991a1f666c3
Dreyson <dreyson@email.sc.edu>
2022-10-19 14:08:45 -0400

    transfer between screens using the menu works.

--- app/src/main/java/com/example/tracknba/Chat.java
+++ app/src/main/java/com/example/tracknba/Chat.java
@@ -1,10 +1,7 @@
 package com.example.tracknba;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
